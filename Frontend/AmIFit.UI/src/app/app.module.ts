8a39593045d3fb7ea0baf70da33c1092dc819f78
Harsh Arora <harsh.arora@siemens-healthineers.com>
2023-04-03 14:11:02 +0530

    Add component for splash, login and dashboard pages

--- Frontend/AmIFit.UI/src/app/app.module.ts
+++ Frontend/AmIFit.UI/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SplashScreenComponent } from './splashscreen/splash-screen/splash-screen.component';
+import { LoginPageComponent } from './login/login-page/login-page.component';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashScreenComponent,
+    LoginPageComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
